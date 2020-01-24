0639d4c39fb0ffe832effad3ec7f32d30371410e
Sureshkumar Karuppusamy <suresh@arbaan.com>
2020-01-24 12:47:05 +0530

    create final build to deploy

---
+++ build/precache-manifest.937689cc6db38ed121c9ab47c7aa959a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb1105ee524698cd046babda61fe551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693a5abd8b8161c09479",
+    "url": "/static/css/main.438a1bd4.chunk.css"
+  },
+  {
+    "revision": "c368b8c98e1aab8b03bb",
+    "url": "/static/js/2.87d70f29.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/static/js/2.87d70f29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "693a5abd8b8161c09479",
+    "url": "/static/js/main.034db747.chunk.js"
+  },
+  {
+    "revision": "2c9ce4e8565854205b6a",
+    "url": "/static/js/runtime-main.b7df4874.js"
+  }
+]);
