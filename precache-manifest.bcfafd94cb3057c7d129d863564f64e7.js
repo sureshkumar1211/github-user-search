0d0432bb4aa7219ad64b8ced1583afd5709f41f7
Sureshkumar Karuppusamy <suresh@arbaan.com>
2020-01-29 19:58:20 +0530

    Updates

---
+++ precache-manifest.bcfafd94cb3057c7d129d863564f64e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5915f656e69a6d52f288cf93933fdc51",
+    "url": "/github-user-search/index.html"
+  },
+  {
+    "revision": "bddd0ebb3cb2df6035b8",
+    "url": "/github-user-search/static/css/main.438a1bd4.chunk.css"
+  },
+  {
+    "revision": "c368b8c98e1aab8b03bb",
+    "url": "/github-user-search/static/js/2.87d70f29.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/github-user-search/static/js/2.87d70f29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bddd0ebb3cb2df6035b8",
+    "url": "/github-user-search/static/js/main.920a2ba6.chunk.js"
+  },
+  {
+    "revision": "096bed6d9b2c1834af93",
+    "url": "/github-user-search/static/js/runtime-main.d5103a66.js"
+  }
+]);
